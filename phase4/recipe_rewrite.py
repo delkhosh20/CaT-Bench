74d69be98a7dcceaa641bec4c63d2d1f6d330adc
delkhosh20 <asal.delkhosh20@gmail.com>
2025-04-02 19:07:34 -0400

    note: deps for Almond_and_apple_cake before change

--- phase4/recipe_rewrite.py
+++ phase4/recipe_rewrite.py
@@ -1,4 +1,11 @@
+from itertools import permutations
+
+def remove_duplicates_ordered(lst):
+    seen = set()
+    return [t for t in lst if not (t in seen or seen.add(t))]
+
 dic = {}
+step = {}
 def format_recipe_steps():
     steps = {}
     
@@ -20,24 +27,61 @@ def format_recipe_steps():
     for i, (key, words) in enumerate(sorted(steps.items()), start=1):
         dic[f"{key[0]} {key[1]}"] = i
         sentence = " ".join(words).replace(' .', '.')
+        step[i] = sentence
         formatted_steps.append(f"{i}. {sentence}")
     
-    return "\n".join(formatted_steps)
+    return formatted_steps
 
-list_dep = []
 def find_dep_steps():
+    list_dep = []
     with open("deps.flow", "r") as file:
         for line in file:
             listID = line.strip("\n").split(" ")
             strid = f"{listID[0]} {listID[1]}"
             strid2 = f"{listID[-3]} {listID[-2]}"
             list_dep.append((dic[strid], dic[strid2]))
+    
+    list_dep = remove_duplicates_ordered(list_dep)
+    
+    index = 0
+    while True:
+        if index+1 == len(list_dep):
+            break
+        
+        current = list_dep[index]
+        next = list_dep[index+1]
+        
+        if current[-1] == next[0]:
+            merged = current + next[1:]
+            list_dep = list_dep[:index] + [merged] + list_dep[index+2:]
+        else:
+            index += 1
+    
+    return list_dep     
 
-            
-# Example usage
-# data = """(Your input data here)"""
+def move_elements(rules):
+    rules = [(1), (2), (3)] + [rules[0]] + [(10)] + [rules[1]]
+    return list(permutations(rules))
+
+def extract_numbers(input_tuple):
+    result = []
+    for item in input_tuple:
+        if isinstance(item, tuple):  # Check if item is a tuple
+            result.extend(extract_numbers(item))  # Recursively extract numbers
+        else:
+            result.append(item)  # If it's a number, add it to the result
+    return result
+                
 output = format_recipe_steps()
 output2 = find_dep_steps()
-print(output)
-print(dic)
-print(list_dep)
+output3 = move_elements(output2)
+# print(output2)
+# print(step)
+n = 0
+with open("Almond_and_apple_cake.txt", "w") as file:
+    for out in output3:
+        file.write("========\n")
+        n +=1
+        for key in extract_numbers(out):
+            file.write(f"{key}. {step[key]}\n")
+    file.write(f"\n number of recipes: {n}")
