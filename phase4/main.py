56db222d5ad8734f23580c89096909efa9e46904
delkhosh20 <asal.delkhosh20@gmail.com>
2025-04-03 22:10:04 -0400

    update: 10 recipes

--- phase4/main.py
+++ phase4/main.py
@@ -1,5 +1,5 @@
 with open("deps.flow", "w") as deps:
-    with open("Best_crispy_roast_potatoes.flow", "r") as file:
+    with open("Black_Bean_and_Sweetcorn_Salad.flow", "r") as file:
         for line in file:
             arry = line.strip("\n").split(" ")
             if (arry[1] != arry[-2] or arry[0] != arry[-3]) and (arry[3] == 't' or arry[3] == 'd'):
